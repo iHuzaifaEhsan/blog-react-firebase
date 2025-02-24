65d1c2b3a4ab10fa68fe1e76baaedb88edd43000
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2025-02-24 14:02:08 +0500

    pages content

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
-import { Button } from '@material-tailwind/react'
-import './App.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Blog from "./pages/blog/Blog";
+import AllBlogs from "./pages/allBlogs/AllBlogs";
+import NoPage from "./pages/nopage/NoPage";
+import BlogInfo from "./pages/blogInfo/BlogInfo";
+import AdminLogin from "./pages/admin/adminLogin/AdminLogin";
+import Dashboard from "./pages/admin/dashboard/Dashboard";
 
 function App() {
-
   return (
-    <>
-     <div className='flex justify-center items-center h-screen'>
-      <Button>Click</Button>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/allblogs" element={<AllBlogs />} />
+          <Route path="/bloginfo/:id" element={<BlogInfo />} />
+          <Route path="/adminlogin" element={<AdminLogin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/*" element={<NoPage />} />
+        </Routes>
+      </Router>
     </div>
-    </>
   )
 }
 
