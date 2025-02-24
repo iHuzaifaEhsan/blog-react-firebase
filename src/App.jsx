c09ca8017e2b95d51bb6289de466e9d179681e5c
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2025-02-24 13:49:43 +0500

    components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Button } from '@material-tailwind/react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className='flex justify-center items-center h-screen'>
+      <Button>Click</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import withMT from "@material-tailwind/react/utils/withMT";
+
+// https://vite.dev/config/
+export default withMT(
+  defineConfig({
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [react(), tailwindcss(),],
+  })
+);
+
